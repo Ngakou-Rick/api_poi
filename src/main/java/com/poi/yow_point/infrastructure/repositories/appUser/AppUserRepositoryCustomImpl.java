8f473c35975e6ac94dc1424c5155239a646769cc
Ngakou-Rick <rickvarnel@gmail.com>
2025-11-28 11:46:08 +0100

    Refactoring ApUser Requests

--- src/main/java/com/poi/yow_point/infrastructure/repositories/appUser/AppUserRepositoryCustomImpl.java
+++ src/main/java/com/poi/yow_point/infrastructure/repositories/appUser/AppUserRepositoryCustomImpl.java
@@ -3,15 +3,14 @@ package com.poi.yow_point.infrastructure.repositories.appUser;
 import java.util.UUID;
 
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.relational.core.query.Criteria;
+import org.springframework.data.relational.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.poi.yow_point.infrastructure.entities.AppUser;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import org.springframework.data.relational.core.query.Criteria;
-import org.springframework.data.relational.core.query.Query;
 
 @Repository
 class AppUserRepositoryImpl implements AppUserRepositoryCustom {
@@ -38,25 +37,28 @@ class AppUserRepositoryImpl implements AppUserRepositoryCustom {
 
     @Override
     public Mono<Boolean> existsByOrgId(UUID orgId) {
+        // CORRECTION ICI : "org_id" -> "organization_id"
         return template.exists(
-                Query.query(Criteria.where("org_id").is(orgId)),
+                Query.query(Criteria.where("organization_id").is(orgId)),
                 AppUser.class);
     }
 
     @Override
     public Mono<Long> countActiveUsersByOrgId(UUID orgId) {
+        // CORRECTION ICI : "orgId" -> "organization_id"
         return template.count(
                 Query.query(
-                        Criteria.where("org_id").is(orgId)
+                        Criteria.where("organization_id").is(orgId)
                                 .and("is_active").is(true)),
                 AppUser.class);
     }
 
     @Override
     public Flux<AppUser> findByOrgIdAndIsActive(UUID orgId, Boolean isActive) {
+        // CORRECTION ICI : "org_id" -> "organization_id"
         return template.select(
                 Query.query(
-                        Criteria.where("org_id").is(orgId)
+                        Criteria.where("organization_id").is(orgId)
                                 .and("is_active").is(isActive)),
                 AppUser.class);
     }
@@ -67,5 +69,4 @@ class AppUserRepositoryImpl implements AppUserRepositoryCustom {
                 Query.query(Criteria.where("role").is(role)),
                 AppUser.class);
     }
-
 }
